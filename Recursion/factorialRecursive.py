6d3269db16e262300930c724cfedf479bd332bcf
Dev <dev@Devs-MacBook-Pro.local>
2023-08-28 18:16:34 +0500

    Day-1, Recursive

---
+++ Recursion/factorialRecursive.py
@@ -0,0 +1,32 @@
+# def recursiveFunction(num):
+#     if Base Condition:
+#         return ----
+#     else:
+#         Recursive Condition
+#         return num + accumulativeSum(num-1)
+
+
+import sys
+
+
+def factorial(num):
+    if (num == 0):
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return num * factorial(num-1)
+
+
+def accumulativeSum(num):
+    if (num == 0):
+        return 0
+    else:
+        return num + accumulativeSum(num-1)
+
+
+print('Factorial is -> ', factorial(5))
+print('Accumulative Sum is -> ', accumulativeSum(5))
+
+
+print(sys.getrecursionlimit())
